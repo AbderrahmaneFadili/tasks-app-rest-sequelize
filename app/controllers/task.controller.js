b76d903075c750a54f9b3a6f300b8c61ebe48216
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-03-01 20:13:25 +0100

    Create Task

--- app/controllers/task.controller.js
+++ app/controllers/task.controller.js
@@ -1,9 +1,34 @@
 const db = require("../models");
 const Task = db.tasks;
+//Operator
 const operator = db.Sequelize.Op;
 
 //Create a Task
-exports.create = () => {};
+exports.create = (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+
+  //Create a task
+  const task = {
+    title: req.body.title,
+    completed: req.body.completed,
+  };
+
+  Task.create(task)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.statue(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial.",
+      });
+    });
+};
 
 //Retrieve all tasks
 exports.findAll = (req, res) => {
--- app/routes/task.routes.js
+++ app/routes/task.routes.js
@@ -2,6 +2,8 @@ module.exports = (app) => {
   const tasks = require("../controllers/task.controller");
   let router = require("express").Router();
 
+  //Create a task
+  router.post("/", tasks.create);
   //Get All tasks
   router.get("/", tasks.findAll);
 
